49ca61f8706d5258c37ee01f03733ca0881a404d
Roman Inflianskas <infroma@gmail.com>
2018-09-30 10:42:01 +0300

    Use upstream aioify

--- setup.py
+++ setup.py
@@ -8,14 +8,11 @@ package_data = \
 {'': ['*']}
 
 install_requires = \
-['aioify', 'jira>=2.0,<3.0']
-
-dependency_links = \
-['git+ssh://git@github.com/rominf/aioify.git']
+['aioify>=0.1.3,<0.2.0', 'jira>=2.0,<3.0']
 
 setup_kwargs = {
     'name': 'aiojira',
-    'version': '0.1.3',
+    'version': '0.1.4',
     'description': 'Asynchronous Jira library',
     'long_description': "# aiojira - asynchronous Jira Python library\nAsynchronous Jira Python library - asynchronous wrapper for https://github.com/pycontribs/jira\n\n## Installation\nTo install from PyPI: https://pypi.org/project/aiojira/ run:\n```shell\n$ pip install aiojira\n```\n\n## Usage\nFor documentation refer to https://jira.readthedocs.io, because `aiojira` uses the same API as `jira` with 2 exceptions:\n1. All functions are converted to coroutines, that means you have to add `await` keyword before all function calls.\n2. To asynchronously create classes from `aiojira` use static method `create`.\n\nExample:\n```python\nimport asyncio\nimport aiojira\n\n\nserver = 'https://jira.example.com/'\nauth = ('user', 'password')\n\nJIRA = asyncio.run(aiojira.JIRA.create(server=server, basic_auth=auth))\nprojects = asyncio.run(JIRA.projects())\nprint(projects)\n```\n",
     'author': 'Roman Inflianskas',
@@ -24,7 +21,6 @@ setup_kwargs = {
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
-    'dependency_links': dependency_links,
     'python_requires': '>=3.6,<4.0',
 }
 
