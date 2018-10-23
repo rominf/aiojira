f1aa2fd61ead4264cdc3c48d9dea37c7db06937b
Roman Inflianskas <infroma@gmail.com>
2018-10-23 10:56:37 +0300

    Skip Issue, Project aiofication

--- aiojira/__init__.py
+++ aiojira/__init__.py
@@ -4,10 +4,10 @@ from aioify import aioify
 import jira
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
-aiojira = aioify(obj=jira, name=__name__)
+aiojira = aioify(obj=jira, name=__name__, skip=['Issue', 'Project'])
 
 sys.modules[__name__] = aiojira
 from aiojira import *
