95824b3a985597b1feccc5089fb9a2ddd71eb8b7
Roman Inflianskas <infroma@gmail.com>
2018-11-14 22:20:56 +0300

    Use poetry-version to get __version__

--- aiojira/__init__.py
+++ aiojira/__init__.py
@@ -2,9 +2,10 @@ import sys
 
 from aioify import aioify
 import jira
+import poetry_version
 
 
-__version__ = '0.1.1'
+__version__ = poetry_version.extract(source_file=__file__)
 
 
 aiojira = aioify(obj=jira, name=__name__, skip=['Issue', 'Project'])
